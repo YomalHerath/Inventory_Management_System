674f30ad8b86f60a32809248a88b2e7b479fed94
Yomal Herath <yomalherath1121@gmail.com>
2021-08-06 09:02:01 +0530

    create header file of the system

---
+++ view/php/views/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Wayamba Archery Suppliers</title>
+</head>
+<header>
+    <link rel="stylesheet" type="text/css" href="../../css/index.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- import font awesome icons -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+</header>
+
+<!--Navigation Bar-->
+<nav class="navbar navbar-expand navbar-dark" style="background-color : #1B4F72;">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link text-light" href="../views/index.php"><b class="text-uppercase">Dashboard</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="../views/supplier_details.php"><b class="text-uppercase">Suppliers</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="../views/product_details.php"><b class="text-uppercase">Products</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="../views/sales_details.php"><b class="text-uppercase">Sales</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="../views/tender_details.php"><b class="text-uppercase">Tenders</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="../forms/logout.php"><b class="text-uppercase">Logout</b></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+</html>
