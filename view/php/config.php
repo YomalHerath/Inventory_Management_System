390a1ab42ef330f4c0ddd5a014b9b1031ac4ab96
Dilki-Weerasekara <you@example.com>
2021-08-07 00:19:06 +0530

    add config file to connect db

---
+++ view/php/config.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost:3325";
+$username = "root";
+$password = "";
+$dbname = "inventory_management_system";
+
+// Creating a connection
+$con = new mysqli($servername, $username, $password, $dbname);
+
+// Checking the connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
+// Connection successful message
+//echo "<script>alert('Connected successfully!')</script>";
