a849da1845e6696f3e2d1fcb1970360e8f608407
Dilki-Weerasekara <you@example.com>
2021-08-07 00:19:47 +0530

    create supplier details page

---
+++ view/php/views/supplier_details.php
@@ -0,0 +1,92 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
+
+?>
+
+    <?php include('../views/header.php'); ?>
+
+    <body>
+
+        <div class="main-panel">
+            <div class="content-wrapper">
+                <div class="col-lg grid-margin stretch-card mx-auto">
+                    <div class="m-3">
+
+                        <div class="col-lg-12 mx-auto">
+                            <div class="m-3">
+                                <h1 class="d-flex justify-content-center text-uppercase"><b>Supplier Details</b></h1>
+                                <div class="card-body">
+                                    <!-- Add Supplier Button -->
+                                    <a class="btn btn-primary col-lg-2 ml-2" href="../forms/add_supplier.php"><i class="fa fa-users" aria-hidden="true"><span class="p-3">Add New Supplier</span></i></a>
+                                    <br>
+                                    <form>
+                                        <div class="row no-gutters align-items-center mt-2 pt-2">
+                                            <!--end of col-->
+                                            <div class="col-lg-3">
+                                                <input class="form-control" type="search" placeholder="Search...">
+                                            </div>
+                                            <!--end of col-->
+                                            <div class="col-auto">
+                                                <button class="btn btn btn-primary" type="submit"><i class="fa fa-search" aria-hidden="true"><span class="p-2">Search</span></i></button>
+                                            </div>
+                                            <!--end of col-->
+                                        </div>
+                                    </form>
+
+                                    <!-- Supplier details table view -->
+                                    <div class="table-responsive pt-3">
+                                        <table class="table table-bordered">
+
+                                            <thead class="table-primary text-center">
+                                                <tr>
+                                                    <th>Supplier ID</th>
+                                                    <th>Company Name</th>
+                                                    <th>Address</th>
+                                                    <th>Email</th>
+                                                    <th>Contact Number</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody class="table-light text-center">
+                                                <td>1221</td>
+                                                <td>Hoyt Archery</td>
+                                                <td>593 North Wright Brothers Drive Salt Lake City, UT 84116</td>
+                                                <td>csteam@hoyt.com</td>
+                                                <td>8013632990</td>
+                                                <td>
+                                                    <?php
+                                                    echo "<a class='btn btn-primary btn-sm' href=\"../views/view_supplier_details.php\"><i class='fa fa-eye' aria-hidden='true'><span class='p-2'>View</span></i></a>";
+                                                    ?>
+                                                    <?php
+                                                    echo "<a class='btn btn-warning btn-sm' href=\"../forms/update_supplier.php\"><i class='fa fa-pencil' aria-hidden='true'><span class='p-2'>Update</span></i></a>";
+                                                    ?>
+                                                </td>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </body>
+    <?php include('../views/footer.php'); ?>
+
+<?php
+
+} else {
+
+    header("location: ../forms/login.php");
+    exit();
+}
+
+?>
