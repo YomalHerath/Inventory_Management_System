869973dde0742e3e545ce8e052340c75b5ef8ddf
Dilki-Weerasekara <you@example.com>
2021-08-07 00:23:18 +0530

    create login control page

---
+++ view/php/form_controllers/login_control.php
@@ -0,0 +1,47 @@
+<?php
+
+session_start();
+
+// get db connection from congfig file
+require '../config.php';
+
+if (isset($_POST['submit'])) {
+
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+
+        function validate($data)
+        {
+
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        $uname = validate($_POST['username']);
+        $pass = ($_POST['password']);
+
+        $result = mysqli_query($con, "SELECT * FROM user WHERE username= '$uname' AND password ='$pass' ");
+
+        $row = mysqli_fetch_array($result);
+        $username = $row['username'];
+        $password = $row['password'];
+        $id = $row['id'];
+
+
+        if (($uname === $username) && ($pass === $password)) {
+
+            $_SESSION['username'] = $username;
+            $_SESSION['password'] = $password;
+            $_SESSION['id'] = $id;
+
+            header("location: ../views/index.php ");
+            exit();
+        } else {
+
+            echo 'Incorrect Username or Password';
+            header("location: ../forms/login.php ");
+            exit();
+        }
+    }
+}
