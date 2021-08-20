331c0386e6e4b3708b5975dac53dd2699e6656cf
Dilki-Weerasekara <dilki.hashi@gmail.com>
2021-08-20 11:38:50 +0530

    get DB values to update supplier form

--- view/php/forms/update_supplier.php
+++ view/php/forms/update_supplier.php
@@ -10,6 +10,31 @@ if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
 
     <body>
 
+        <?php
+
+        // get db connection from congfig file
+        require '../config.php';
+
+        //getting title from url
+        if (!isset($_GET['supplier_id'])) {
+            header("Location: ../views/supplier_details.php");
+        } else {
+            $supplier_id = $_GET['supplier_id'];
+        }
+
+        //selecting data associated
+        $result = mysqli_query($con, "SELECT * FROM suppliers WHERE supplier_id = '$supplier_id' ");
+
+        while ($row = mysqli_fetch_array($result)) {
+            $supplier_id = $row['supplier_id'];
+            $company_name = $row['company_name'];
+            $company_address = $row['company_address'];
+            $email = $row['email'];
+            $contact_no = $row['contact_no'];
+        }
+
+        ?>
+
         <div class="main-panel">
             <div class="content-wrapper">
                 <div class="col-lg grid-margin stretch-card mx-auto">
@@ -17,32 +42,32 @@ if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
 
                         <div class="col-lg-4 mx-auto">
                             <div class="card mb-3">
-                                <div class="card-header"style="background-color:#e0b0ff;">
+                                <div class="card-header">
                                     <h2 class="card-title d-flex justify-content-center text-uppercase"><b>Update Supplier Details</b></h2>
                                     <div class="card-body">
                                     </div>
                                     <form class="m-1" action="../form_controllers/submit_updated_supplier.php" method="POST" enctype="multipart/form-data">
 
-                                        <input type="hidden" class="form-control" name="supplier_id" id="supplier_id">
+                                        <input type="hidden" class="form-control" name="supplier_id" id="supplier_id" value="<?php echo $supplier_id; ?>">
 
                                         <div class="form-group p-2">
                                             <label for="exampleFormControlInput1">Company Name</label>
-                                            <input type="text" class="form-control" id="company_name" name="company_name" required>
+                                            <input type="text" class="form-control" id="company_name" name="company_name" value="<?php echo $company_name; ?>" required>
                                         </div>
                                         <div class=" form-group p-2">
                                             <label for="exampleFormControlInput1">Address</label>
-                                            <input type="text" class="form-control" id="company_address" name="company_address" required>
+                                            <input type="text" class="form-control" id="company_address" name="company_address" value="<?php echo $company_address; ?>" required>
                                         </div>
                                         <div class=" form-group p-2">
                                             <label for="exampleFormControlInput1">Email</label>
-                                            <input type="text" class="form-control" id="email" name="email" required>
+                                            <input type="text" class="form-control" id="email" name="email" value="<?php echo $email; ?>" required>
                                         </div>
                                         <div class=" form-group p-2">
                                             <label for="exampleFormControlInput1">Contact No</label>
-                                            <input type="text" class="form-control" id="contact_no" name="contact_no" required>
+                                            <input type="text" class="form-control" id="contact_no" name="contact_no" value="<?php echo $contact_no; ?>" required>
                                         </div>
                                         <br>
-                                        <button type="submit" name="submit" class="btn btn btn-success m-1">Save</button>
+                                        <button type="submit" name="submit" class="btn btn btn-success m-1">Update</button>
                                         <button type=" button" class="btn btn btn-danger m-1" onclick="history.go(-1);">Cancel</button>
                                     </form>
                                 </div>
